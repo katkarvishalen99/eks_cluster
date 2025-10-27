1c1650b13e1479e5d23b03ae8576960b22466b1d
katkarvishalen99 <121720944+katkarvishalen99@users.noreply.github.com>
2025-10-27 08:30:49 +0000

    other files

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "cidr_subnet_a" {
+  type = string
+}
+
+variable "cidr_subnet_b" {
+  type = string
+}
+
+variable "instnace_type" {
+  type = string
+}
+
+variable "image_id" {
+  type = string
+}
+
+variable "eks_version" {
+  type = string
+}
